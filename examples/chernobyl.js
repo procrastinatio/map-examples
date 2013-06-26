223dc9ab6ce2555f9f278aa7f723ace4912da1f9
Marc Monnerat <marc.monnerat@bluewin.ch>
2013-06-26 20:38:05 +0200

    cosmetics

--- examples/chernobyl.js
+++ examples/chernobyl.js
@@ -4,10 +4,10 @@ var _map;
 function init() {
     _api = new GeoAdmin.API();
     _map = _api.createMap({
-        div: "map",
+        div: 'map',
         bgLayer: 'ch.swisstopo.pixelkarte-grau'
     });
-    var layer = createLayer("BLN with style and labels", "data/chernobyl.kml");
+    var layer = createLayer('BLN with style and labels', 'data/chernobyl.kml');
 
     var drag = new OpenLayers.Control.DragFeature(layer);
 
@@ -21,43 +21,47 @@ function init() {
     var controls = _map.getControlsByClass('OpenLayers.Control.SelectFeature');
     for (var i = 0; i < controls.length; ++i) {
         controls[i].deactivate();
-             
+
     }
-     _map.zoomToExtent([568546.15384615, 197880.76923077, 706046.15384615, 285380.76923077]);
+     _map.zoomToExtent([568546.15384615,
+                        197880.76923077,
+                        706046.15384615,
+                        285380.76923077]);
 }
 
 function createLayer(name, path) {
 
     var styleMap = new OpenLayers.StyleMap({
         'default': new OpenLayers.Style({
-            label: "${name}",
-            fontWeight: "bold",
-            fontColor: "black",
-            fontSize: "11px",
-            fontFamily: "Voltaire",
+            label: '${name}',
+            fontWeight: 'bold',
+            fontColor: 'black',
+            fontSize: '11px',
+            fontFamily: 'Voltaire',
             //labelAlign: "lb",
             pointRadius: 2,
-            labelXOffset: "${getOffset}",
+            labelXOffset: '${getOffset}',
             // "${labelX}",
-            labelYOffset: "${getOffset}",
+            labelYOffset: '${getOffset}',
             //"${labelY}",
             labelSelect: true,
-            labelOutlineColor: "white",
+            labelOutlineColor: 'white',
             labelOutlineWidth: 3,
-            strokeColor: "${getFillColor}",
+            strokeColor: '${getFillColor}',
             strokeOpacity: 0.8,
             strokeWidth: 3,
-            fillColor: "${getFillColor}",
+            fillColor: '${getFillColor}',
             fillOpacity: 0.5,
             pointRadius: 10,
             externalGraphic: 'data/tchernobyl.png'
         }, {
             context: {
-                getFillColor: function (feature) {
+                getFillColor: function(feature) {
 
-                    return feature.attributes.name == 'Border 30 km exclusion zone' ? '#b43278' : '#d20014';
+                    return feature.attributes.name ==
+                        'Border 30 km exclusion zone' ? '#b43278' : '#d20014';
                 },
-                getOffset: function (feature) {
+                getOffset: function(feature) {
                     return -0.8 * feature.attributes.offset;
                 }
             }
@@ -73,9 +77,9 @@ function createLayer(name, path) {
             format: new OpenLayers.Format.KML({
                 extractStyles: false,
                 extractAttributes: true,
-                externalProjection: new OpenLayers.Projection("EPSG:4326"),
+                externalProjection: new OpenLayers.Projection('EPSG:4326'),
                 internalProjection: _map.projection,
-                kmlns: "http://www.opengis.net/kml/2.2",
+                kmlns: 'http://www.opengis.net/kml/2.2'
             })
         })
     });
